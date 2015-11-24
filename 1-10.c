30e5705eb67442f2cba6a7b4c8da45a66cc1c073
kti1234 <113218217@qq.com>
2015-11-24 16:23:38 +0800

    Create 1-10.c

---
+++ 1-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+        int c;
+
+        while((c = getchar()) != EOF){
+                if(c == '\t')
+                        printf("\\t");
+                else if(c == '\b')
+                        printf("\\b");
+                else if(c == '\\')
+                        printf("\\\\");
+                else
+                        putchar(c);
+        }
+
+}
